aee2fa4ff1ce2de0607f1d97aada4a22a445ac20
EMQX-YangM <yangm@emqx.io>
2022-01-06 00:07:07 +0800

    setp1

--- src/Type.hs
+++ src/Type.hs
@@ -9,20 +9,29 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Type where
 
 import           Control.Algebra
+import           Control.Carrier.Fresh.Strict
 import           Control.Carrier.Reader
 import           Control.Carrier.State.Strict
+import           Control.Concurrent
 import           Control.Concurrent.STM
 import           Control.Effect.Labelled
-import           Control.Effect.Labelled        ( HasLabelled )
 import           Control.Exception
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Data.IORef
+import qualified Data.IntMap                   as IntMap
+import           Data.IntMap                    ( IntMap )
 import           Data.Kind
+-- import           Data.Map                       ( Map )
+-- import qualified Data.Map                      as Map
 
 
 data BaseAction :: Type -> Type -> (Type -> Type) -> Type -> Type where
@@ -39,7 +48,7 @@ putOutput output = sendLabelled @BaseAction (PutOutput output)
 type Counter = IORef Int
 
 newtype BaseActionC input output m a = BaseActionC {unBaseActionC :: ReaderC (TChan input, Counter, TChan output, Counter) m a}
-    deriving (Functor, Applicative, Monad)
+    deriving (Functor, Applicative, Monad, MonadIO)
 
 instance (Algebra sig m, MonadIO m) => Algebra (BaseAction input output :+: sig) (BaseActionC input output m) where
     alg hdl sig ctx = BaseActionC $ ReaderC $ \(input, ic, output, oc) ->
@@ -60,21 +69,97 @@ instance (Algebra sig m, MonadIO m) => Algebra (BaseAction input output :+: sig)
                 signa
                 ctx
 
+
+runBaseActionC
+    :: TChan input
+    -> Counter
+    -> TChan output
+    -> Counter
+    -> Labelled BaseAction (BaseActionC input output) m a
+    -> m a
+runBaseActionC inp ci out co f =
+    runReader (inp, ci, out, co) $ unBaseActionC $ runLabelled f
+
 type WorkName = String
 
+data Command = Command
+
 loop
-    :: ( HasLabelled BaseAction (BaseAction input output) sig m
-       , Has (Reader WorkName) sig m
-       , MonadIO m
-       )
-    => (input -> IO output)
+    :: (HasLabelled BaseAction (BaseAction input output) sig m, MonadIO m)
+    => WorkName
+    -> (input -> IO output)
     -> m ()
-loop fun = forever $ do
+loop workName fun = forever $ do
     input <- getInput
     val   <- liftIO $ try @SomeException $ fun input
     case val of
         Left se -> do
-            name <- ask @WorkName
+            let name = workName
             liftIO $ appendFile name (show se)
         Right output -> putOutput output
 
+startThread
+    :: ( Has
+             (State (IntMap (WorkName, ThreadId, Counter, Counter)) :+: Fresh)
+             sig
+             m
+       , MonadIO m
+       )
+    => WorkName
+    -> (input -> IO output)
+    -> (TChan input, Counter)
+    -> m (TChan output, Counter)
+startThread name fun (inp, ci) = do
+    out  <- liftIO newTChanIO
+    co   <- liftIO $ newIORef 0
+    thid <- liftIO $ forkIO $ void $ runBaseActionC inp ci out co $ loop
+        name
+        fun
+    number <- fresh
+    modify (IntMap.insert number (name, thid, ci, co))
+    pure (out, co)
+
+
+data Flow a b where
+     Source ::IO a -> Flow a b -> Flow () b
+     Pipe ::(a -> IO b) -> Flow b c -> Flow a c
+     Sink ::Flow a b -> (b -> IO ()) -> Flow a ()
+
+type Work = Flow () ()
+
+
+-- runWork
+--     :: ( Has (State (IntMap (WorkName, ThreadId, Counter)) :+: Fresh) sig m
+--        , MonadIO m
+--        )
+--     => (forall a b . Flow a b -> TChan a -> m (TChan b))
+-- runWork = \case
+--     Source io fl -> \_ -> liftIO $ do
+--         tc1 <- newTChanIO
+--         forkIO $ void $ forever $ do
+--             a <- io
+--             atomically $ writeTChan tc1 a
+--         tc2 <- newTChanIO
+--         runWork fl tc1 
+--         pure tc2
+--     Pipe f  fl -> undefined
+--     Sink fl f  -> undefined
+
+
+-- runFun ::  -> IO ()
+-- runFun name fun (inp, ci) = do
+--     out <- newTChanIO
+--     co  <- newIORef 0
+--     -- runLoop inp ci out co name fun
+--     undefined
+
+-- t1 = runFun "t1" fun1
+-- t2 = runFun "t2" fun2
+
+-- fun1 :: Int -> IO [String]
+-- fun1 i = replicateM i getLine
+
+-- fun2 :: [String] -> IO String
+-- fun2 i = pure (concat i)
+
+-- data FlowType a b  = SourceType a | WorkType a b | SinkType b
